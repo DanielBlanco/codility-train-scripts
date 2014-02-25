b64890a6f0780eb3f460909c1ae4f51b6709fffa
Daniel Blanco <dblanco@growthaccelerationpartners.com>
2014-02-25 14:43:10 -0600

    Adding perm_check

--- counting_elements.rb
+++ counting_elements.rb
@@ -17,7 +17,81 @@ class CountingElements
     count
   end
 
+  # A non-empty zero-indexed array A consisting of N integers is given.
+  # A permutation is a sequence containing each element from 1 to N once, and only once.
+  #
+  # For example, array A such that:
+  #     A[0] = 4
+  #     A[1] = 1
+  #     A[2] = 3
+  #     A[3] = 2
+  # is a permutation, but array A such that:
+  #     A[0] = 4
+  #     A[1] = 1
+  #     A[2] = 3
+  # is not a permutation.
+  #
+  # The goal is to check whether array A is a permutation.
+  # Write a function:
+  #
+  #   def solution(a)
+  #
+  # that, given a zero-indexed array A, returns 1 if array A is a permutation and 0 if it is not.
+  # For example, given array A such that:
+  #     A[0] = 4
+  #     A[1] = 1
+  #     A[2] = 3
+  #     A[3] = 2
+  # the function should return 1.
+  # Given array A such that:
+  #     A[0] = 4
+  #     A[1] = 1
+  #     A[2] = 3
+  # the function should return 0.
+  #
+  # Assume that:
+  #   N is an integer within the range [1..100,000];
+  #   each element of array A is an integer within the range [1..1,000,000,000].
+  # Complexity:
+  #   expected worst-case time complexity is O(N);
+  #   expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+  #
+  # @return [Integer] 1 if the array is a permutation, 0 if it is not.
+  def perm_check(a)
+    puts 'perm_check'
+    n = a.size            # Get array a size
+    count = [0] * n       # Create an array of size n filled with 0s (zeros).
+    for i in 0..(a.size-1)
+      v = a[i]
+      if v <= a.size
+        count[v-1]+=1
+      else
+        return 0
+      end
+    end
+    for i in 0..(count.size-1)
+      return 0 if count[i] != 1
+    end
+    1
+  end
+  # Ruby has the sort method so it is easy to use this method to solve the problem.
+  def perm_check_with_sort(a)
+    puts 'perm_check'
+    a.sort!
+    for i in 0..(a.size-1)
+      return 0 if a[i] != i+1
+    end
+    1
+  end
+
+
+
 end
 
 test = CountingElements.new
 puts test.algorithm([0,0,4,2,4,5], 5).to_s
+puts test.perm_check [1]
+puts test.perm_check [2]
+puts test.perm_check [4,1,3,2]
+puts test.perm_check [4,1,3,2]
+puts test.perm_check_with_sort []
